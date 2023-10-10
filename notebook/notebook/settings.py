1db3b0a7288cc4524e7429072f797ef9afd57115
MariamNsubuga <mariam.nakanyike@gmail.com>
2023-10-10 12:33:04 +0300

    REQUIREMENTS updated the requirements file and readme file

--- notebook/notebook/settings.py
+++ notebook/notebook/settings.py
@@ -193,7 +193,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mariamtestn@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'khwnkmfjwimbddgl'
 EMAIL_USE_SSL = False
 
    
